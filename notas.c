b1f12ce0154834e07757d05d4554fb5f13eae062
salgado <alexsalgado1@gmail.com>
2018-11-02 00:22:12 +0000

    matriz notas

--- notas.c
+++ notas.c
@@ -60,7 +60,44 @@ int main()
         media = ( av1 + av2 ) / 2;
     }
     printf("\nMedia = %.2f", media);
+ 
+ 
+     //imprimir relatorio de alunos
+    printf("\nRelatório de Resultado de Alunos");
+    printf("\n============================================");
+    printf("\nMat\tAV1\tAV2\tAV3\tResultado");
+    printf("\n============================================");
     
+    for(i=0;i<4;i++)
+    {
+        printf("\n%d\t%.2f\t%.2f\t%.2f", i, notas[i][0], notas[i][1], 
+                                                      notas[i][2]);
+                                                      
+         //calcular media
+        av1 = notas[i][0];
+        av2 = notas[i][1];
+        av3 = notas[i][2];
+        
+        if( (av1 < av2 ) && (av1 < av3 ))
+        {
+            media = ( av2 + av3 ) / 2;
+        }
+        else if( av2 < av3)
+        {
+            media = ( av1 + av3 ) / 2;
+        }
+        else
+        {
+            media = ( av1 + av2 ) / 2;
+        }
+        
+        if(media>=6)
+            printf("\tPASSOU");
+        else
+            printf("\tNÃO PASSOU");
+   }
+    
+   
     printf("\n\n");
     return 0;
 }
